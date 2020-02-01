a1461aa85a6ac86d0e15d460c80978310cf488ff
a <a>
2020-01-31 20:14:42 -0700

    Added the sprite & Tank movement

---
+++ Assets/Scripts/TankMover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMover : MonoBehaviour
+{
+    private Transform tf;
+    public float speed = 1.0f;
+    public float turnSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            tf.position += tf.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            tf.position += -tf.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            tf.Rotate(0, 0, -turnSpeed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            tf.Rotate(0, 0, turnSpeed);
+        }
+
+    }
+}
