23a4ec0f2f731d05f62fd8b5a19932010e7c3fb6
Alec <Quaintman2000@gmail.com>
2020-02-07 18:57:05 -0700

    Made Enemy ship a prefab, got them to move, and destroy player if hit

---
+++ Assets/Scripts/EnemyShip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShip : MonoBehaviour
+{
+    public Transform tf;
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tf.position = tf.position + Vector3.right * speed * Time.deltaTime;
+    }
+}
--- Assets/Scripts/TankMover.cs
+++ Assets/Scripts/TankMover.cs
@@ -47,6 +47,8 @@ public class TankMover : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D otherObject)
     {
         Debug.Log("The GameObject of the other object is named: " + otherObject.gameObject.name);
+        // if player runs into the enemy ships, it dies
+        Destroy(this.gameObject);
     }
     private void OnDestroy()
     {
